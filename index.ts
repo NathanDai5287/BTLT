df03a7377db79e8ab50a9b8a4509284cce553953
Nathan Dai <nathandai2000@gmail.com>
2025-09-11 16:12:08 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+const input = require('prompt-sync')();
+
+interface TodoItem {
+	text: string;
+	id: number;
+}
+
+const todo: TodoItem[] = [];
+
+const create = (text: string) => {
+	// TODO
+}
+
+const show = (id: number) => {
+	// TODO
+}
+
+const list = () => {
+	// TODO
+}
+
+const update = (id: number, text: string) => {
+	// TODO
+}
+
+const del = (id: number) => {
+	// TODO
+}
+
+while (true) {
+	const command = input('> ');
+
+	// TODO: parse command and call appropriate function
+
+	if (command === 'exit') {
+		break;
+	}
+}
